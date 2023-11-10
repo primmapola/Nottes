8a5fb1decaa333fedc4b41890793e40deea5c890
dongrigory <dongrigory29@gmail.com>
2023-11-10 19:02:47 +0300

    Delete main storyboard

--- Nottes/Nottes/AppDelegate.swift
+++ Nottes/Nottes/AppDelegate.swift
@@ -11,10 +11,18 @@ import CoreData
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+    
+        let mainViewController = FoldersViewController()
+        
+        let navigationController = UINavigationController(rootViewController: mainViewController)
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+
         return true
     }
 
