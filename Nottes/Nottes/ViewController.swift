87b698701701d177ab9ab67c37afd9a32c1d3c36
dongrigory <dongrigory29@gmail.com>
2023-11-10 18:35:09 +0300

    Add swift project

---
+++ Nottes/Nottes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Nottes
+//
+//  Created by Grigory Don on 10.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
